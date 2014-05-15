ad11b652ab7ce2ea08411121605989646310a6e7
ArtixAllMighty <robbeenaxel@hotmail.com>
2014-05-15 02:12:52 +0200

    added item class

---
+++ src/main/java/net/subaraki/powermasks/item/ItemPowerMask.java
@@ -0,0 +1,29 @@
+package net.subaraki.powermasks.item;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+
+public class ItemPowerMask extends ItemArmor{
+
+	String texture;
+
+	public ItemPowerMask(ArmorMaterial mat, int i,
+			int b, String s) {
+		super(mat, i, b);
+		
+		texture = s;
+	}
+
+	@Override
+	public String getArmorTexture(ItemStack stack, Entity entity, int slot,
+			String type) {
+
+		if(slot == 0){
+			return texture;
+		}
+		
+		return super.getArmorTexture(stack, entity, slot, type);
+	}
+}
