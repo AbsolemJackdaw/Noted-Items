1d7568d18bdfeac16164656de13991c25af53743
ArtixAllMighty <robbeenaxel@hotmail.com>
2014-05-16 13:54:26 +0200

    #44

--- src/main/java/net/subaraki/note/block/NotingTable.java
+++ src/main/java/net/subaraki/note/block/NotingTable.java
@@ -1,5 +1,6 @@
 package net.subaraki.note.block;
 
+import cpw.mods.fml.client.registry.RenderingRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockAnvil;
 import net.minecraft.block.material.Material;
@@ -11,6 +12,15 @@ public class NotingTable extends Block{
 		BlockAnvil b;
 	}
 	
-	
+	@Override
+	public boolean isOpaqueCube() {
+		return false;
+	}
 
+	@Override
+	public int getRenderType() {
+		return RenderingRegistry.getNextAvailableRenderId();
+	}
+	
+	
 }
