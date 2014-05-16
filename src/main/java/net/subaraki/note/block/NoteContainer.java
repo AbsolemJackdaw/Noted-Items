4acfdcafd57352c51a7bfe9ad7ef3d4d7b9cf1c6
ArtixAllMighty <robbeenaxel@hotmail.com>
2014-05-16 15:18:15 +0200

    #49

---
+++ src/main/java/net/subaraki/note/block/NoteContainer.java
@@ -0,0 +1,16 @@
+package net.subaraki.note.block;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+
+public class NoteContainer extends Container {
+
+	public NoteContainer() {
+	}
+
+	@Override
+	public boolean canInteractWith(EntityPlayer var1) {
+		return true;
+	}
+
+}
