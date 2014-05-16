8468994470683c0c484897fdf251fd6d22f15460
ArtixAllMighty <robbeenaxel@hotmail.com>
2014-05-16 13:44:19 +0200

    #43

---
+++ src/main/java/net/subaraki/note/block/NotingTable.java
@@ -0,0 +1,16 @@
+package net.subaraki.note.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockAnvil;
+import net.minecraft.block.material.Material;
+
+public class NotingTable extends Block{
+
+	public NotingTable(Material mat) {
+		super(mat);
+		BlockAnvil b;
+	}
+	
+	
+
+}
